1388561693d5095e9eb75476b78592d06bdc045e
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-05-11 15:20:36 +0300

    Update spawn.cs

--- Game 2/Assets/Scripts/spawn.cs
+++ Game 2/Assets/Scripts/spawn.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class spawn : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    //public int animalIndex;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    int positionX = 20;
+    int positionY = 0;
+    int positionZ = 30;
+    
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.S)){
+
+           int animalIndex = Random.Range(0, 4);
+           Vector3 Positions =new Vector3(Random.Range(-positionX, positionX),positionY,positionZ);
        
-       Instantiate(animalPrefabs[1], new Vector3(0,0,30), animalPrefabs[1].transform.rotation);
+       Instantiate(animalPrefabs[animalIndex], Positions, animalPrefabs[animalIndex].transform.rotation);
 
         }//if
 
