333d0c48b9ea3f69c6673241b21fb5204d14b2dd
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-05-16 03:50:39 +0300

    Update spawn.cs

--- Game 2/Assets/Scripts/spawn.cs
+++ Game 2/Assets/Scripts/spawn.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class spawn : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+   public int positionX = 20;
+    public int positionY = 0;
+   public  int positionZ = 30;
+   public float startDelay = 2;
+   public float spawnInterval = 1.5f;
     //public int animalIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+     InvokeRepeating("randomAnimal", startDelay, spawnInterval);   
     }
-    int positionX = 20;
-    int positionY = 0;
-    int positionZ = 30;
+    
     
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.S)){
 
-           int animalIndex = Random.Range(0, 4);
-           Vector3 Positions =new Vector3(Random.Range(-positionX, positionX),positionY,positionZ);
-       
-       Instantiate(animalPrefabs[animalIndex], Positions, animalPrefabs[animalIndex].transform.rotation);
-
+                randomAnimal();
+         
         }//if
 
 
     }
+    void randomAnimal(){
+
+  int animalIndex = Random.Range(0, 4);
+           Vector3 Positions =new Vector3(Random.Range(-positionX, positionX),positionY,positionZ);
+       
+       Instantiate(animalPrefabs[animalIndex], Positions, animalPrefabs[animalIndex].transform.rotation);
+
+
+    }//randomAnimal
 }
